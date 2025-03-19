a76ef0645234da1da8e0c6e41304fe33f2e367af
Gabriel12326 <2025-0076@unad.edu.do>
2025-03-19 13:02:54 -0400

    Add files via upload

--- 1.py
+++ 1.py
@@ -4,14 +4,13 @@ Escribe un
 El programa debe mostrar todos los números del 1 hasta el número ingresado, 
 uno por uno, utilizando un bucle while."
 """
-while True:
-    
-     numero = int(input("ingrese un numero entero positivo:"))
-     if numero >0:
-      i=1
-      while i>=numero:
-       print(i)
-       i+=1
-      break
-     else:
-       print("el numero tiene que ser positivo.")
+numero = int(input("ingrese un numero entero positivo: "))
+while numero <=0:
+      print("el numero tiene que ser positivo.")
+      numero = int(input("ingrese un numero entero positivo: "))
+   
+
+i=1
+while i<=numero:
+ print(i)
+ i+=1
--- 8.py
+++ 8.py
@@ -4,6 +4,7 @@ donde el valor de n lo ingresa el usuario, utilizando un bucle for."
 """
 n = int(input("Ingrese cuántos números de Fibonacci desea: "))
 a, b = 0,1
+
 for _ in range(n):
  print(a,end = " ") 
  a,b=b,a+b
